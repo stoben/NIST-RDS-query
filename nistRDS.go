d721322cc949a6dd1349f070c67482675875b5ec
Ben M Stokland <stoben@users.noreply.github.com>
2021-06-24 08:42:56 +0200

    add(argument options)

--- nistRDS.go
+++ nistRDS.go
@@ -4,7 +4,8 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"fmt"	
+	"fmt"
+	"strings"
 	"net/http" 
 	"os"
 	"io"
@@ -12,15 +13,40 @@ import (
 )
 
 
+/*
+examples
+nistRDS.go -f file
+nistRDS.go -h 8ED4B4ED952526D89899E723F3488DE4
+
+
+*/
 func main() {
-	md5hash, err := md5FromFile(os.Args[0])
-	if(err != nil) {
-		fmt.Println(err)
+
+	usage := "Usage -f for creating MD5 on the fly -h for setting md5 directly";
+
+	if(len(os.Args) < 2) {
+		fmt.Println(usage)
 		return
 	}
-	
+
+	var md5hash string
+
+	if(os.Args[1] == "-f") {		
+		var err error
+		md5hash, err = md5FromFile(os.Args[2])
+		if(err != nil) {
+			fmt.Println(err)
+			return
+		}
+	} else if(os.Args[1] == "-h") {
+		md5hash = strings.TrimSpace(os.Args[2])
+	} else {
+		fmt.Println(usage) //unknown command
+		return;
+	}
+
 	baseUrl := fmt.Sprintf("https://hashlookup.circl.lu/lookup/md5/%s", md5hash)
-	
+
 	client := &http.Client { }
 
 	req, _ := http.NewRequest("GET", string(baseUrl), nil)
@@ -30,8 +56,10 @@ func main() {
 
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println(string(body))
+	
+	fmt.Println(string(body))
 }
+
 
 
 func md5FromFile(filepath string) (string, error) {
